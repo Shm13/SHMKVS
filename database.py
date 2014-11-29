06f600e3a610e26bdeb7d07c45590861f2cccbab
Nikilay Shamanovich <tadamus93@gmail.com>
2014-11-29 23:54:32 +0300

    database code in separate file

---
+++ database.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Database:
+    def __init__(self, name):
+        self.name = name
+        self.list = {}
+    def create(self, obj, key=0):
+        for x in self.list.keys():
+            if key==x:
+                key+=1
+                continue
+            else:
+                break
+        self.list[key]=obj
+    def read(self, key):
+        return self.list[key]
+    def update(self, obj, key):
+        self.list.update({key:obj})
+    def delete(self, key):
+        del self.list[key]
+
+    def commit(self):
+        # Serialization here:
+        pass
+    def find(self):
+        # Find by object parametrs here:
+        pass
+    pass
--- server.py
+++ server.py
@@ -3,6 +3,9 @@
 
 import threading
 import socketserver
+from database import Database
+
+db = Database('testing')
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
@@ -11,32 +14,6 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         print(self.data)
         self.request.sendall(self.data)
 
-class database:
-    def __init__(self, name):
-        self.name = name
-        self.list = {}
-    def create(self, obj, key=0):
-        for x in self.list.keys():
-            if key==x:
-                key+=1
-                continue
-            else:
-                break
-        self.list[key]=obj
-    def read(self, key):
-        return self.list[key]
-    def update(self, obj, key):
-        self.list.update({key:obj})
-    def delete(self, key):
-        del self.list[key]
-
-    def commit(self):
-        # Serialization here:
-        pass
-    def find(self):
-        # Find by object parametrs here:
-        pass
-    pass
 
 # Debug:
 if __name__ == '__main__':
@@ -44,4 +21,3 @@ if __name__ == '__main__':
 
     server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
     server.serve_forever()
-    pass 
